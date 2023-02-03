3657370b1cc3eb7ab08024c13b4a8686e507ad3e
charliebrockmeier <cbrockmeier2@huskers.unl.edu>
2023-02-03 13:08:28 -0600

    I did stuff for mad

--- src/edu/unl/raikes/git/GitLabHelper.java
+++ src/edu/unl/raikes/git/GitLabHelper.java
@@ -10,6 +10,7 @@ public class GitLabHelper {
     }
 
     public void PrintWelcomeMessage() {
-        System.out.print("This is a lesson on git made by "  + Arrays.toString(persons));
-    }
+        System.out.printf("This is a lesson on git made by: %s",
+        Arrays.toString(persons));
+        }
 }
