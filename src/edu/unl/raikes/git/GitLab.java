55369093a33c1cd7de55b6f4d9b6c10a079d0ca6
madelynpaige15 <mcraft2@huskers.unl.edu>
2023-02-03 12:40:37 -0600

    updated helper function for multiple people

--- src/edu/unl/raikes/git/GitLab.java
+++ src/edu/unl/raikes/git/GitLab.java
@@ -3,7 +3,7 @@ package edu.unl.raikes.git;
 public class GitLab {
 
     public static void main(String[] args) {
-        GitLabHelper helper = new GitLabHelper("Madelyn", "Charlie");
+        GitLabHelper helper = new GitLabHelper(new String[] { "person1", "person2" });
         helper.PrintWelcomeMessage();
     }
 }
