892157ad8dc59b979a0fa566d3c0ed63d0c0082f
madelynpaige15 <mcraft2@huskers.unl.edu>
2023-02-03 12:34:27 -0600

    Changed GitLab to use helper class

--- src/edu/unl/raikes/git/GitLab.java
+++ src/edu/unl/raikes/git/GitLab.java
@@ -3,6 +3,7 @@ package edu.unl.raikes.git;
 public class GitLab {
 
     public static void main(String[] args) {
-        System.out.print("This is a lesson on git");
+        GitLabHelper helper = new GitLabHelper("Madelyn", "Charlie");
+        helper.PrintWelcomeMessage();
     }
 }
